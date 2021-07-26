2f6101ac0380c6fd66ac63b58aaf0c716f7de517
DNL <danylo.zhalkovskyy@icloud.com>
2021-07-26 16:51:50 +0200

    transactions book model

--- EasyHome/settings.py
+++ EasyHome/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'User',
+    'TransactionsBook',
 ]
 
 MIDDLEWARE = [
---
+++ TransactionsBook/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone
+
+
+class TransactionsBook(models.Model):
+    token = models.CharField(max_length=20, null=False)
+    verification_code = models.CharField(max_length=4, null=False)
+    name = models.CharField(max_length=255, null=False)
+    creation_date = models.DateField(default=timezone.now, null=False)
+    description = models.TextField(null=True)
+    author = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
